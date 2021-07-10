24b3c17a7e5525958b3e8a081a701e8533b05b6b
Carlos Herrera <cahe1540@gmail.com>
2021-07-09 20:36:53 -0700

    add some exception handling

--- index.js
+++ index.js
@@ -17,51 +17,61 @@ const data = JSON.parse(json);
 const server = http.createServer((req, res) => {
 
     //first remember how to properly extract the url
-    const pathname = url.parse(req.url,true).pathname;
-    const query = url.parse(req.url,true).query;
-    
+    const pathname = url.parse(req.url, true).pathname;
+    const query = url.parse(req.url, true).query;
+
     //add routing here using if-else-if
-    if(pathname === '/' || pathname === '/overview'){
-        res.writeHead(200, {'Content-type':'text/html'});
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
 
         //generate a card for each element in json file
         const cards = data.map(el => {
-            return replaceTemplate( tmpCard, el);
+            return replaceTemplate(tmpCard, el);
         }).join('');
-    
+
         //update cards to overview 
         const overview = tmpOverview.replace(/{%CARDS%}/g, cards);
 
         //send response
         res.end(overview);
-    
-    }else if(pathname === '/monster'){
-        res.writeHead(200, {'Content-type':'text/html'});
-        
-        const monster = replaceTemplate(tmpMonster, data[query.id]);
 
-        res.end(monster);
-    }else if(pathname === '/api'){
-        res.writeHead(200, {'Content-type':'application/json'});
+    } else if (pathname === '/monster') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+
+
+        if (!query.id) {
+            res.writeHead(404, { 'Content-type': 'text/html' });
+            return res.end(`<h1 style="text-align:center;">NO MONSTER ID WAS PROVIDED!</h1>`);
+        }
+
+        try {
+            const monster = replaceTemplate(tmpMonster, data[query.id]);
+            res.end(monster);
+        } catch (error) {
+            res.writeHead(404, { 'Content-type': 'text/html' });
+            return res.end(`<h1 style="text-align:center;">THAT MONSTER ID DOES NOT EXIST!</h1>`);
+        }
+
+    } else if (pathname === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(json);
-    
-    //correctly render the images
-    }else if(/\.(jpg|jpeg|png|gif)$/i.test(pathname)) {
-        fs.readFile(`${__dirname}/data/img${pathname}`, (err, data) => {
-                res.writeHead(200, { "Content-type": "image/jpeg" });
-                res.end(data);
-            });
 
-    //bad url
-    }else{
-        res.writeHead(404, {'Content-type':'text/html'});
-        res.end(`<h1>URL NOT FOUND!</h1>`);
+        //correctly render the images
+    } else if (/\.(jpg|jpeg|png|gif)$/i.test(pathname)) {
+        fs.readFile(`${__dirname}/data/img${pathname}`, (err, data) => {
+            res.writeHead(200, { "Content-type": "image/jpeg" });
+            res.end(data);
+        });
+
+        //bad url
+    } else {
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end(`<h1 style="text-align:center;">URL NOT FOUND!</h1>`);
     }
 });
 
-const port = 1313;
-const localhost = '127.0.0.1';
+const port = 7000;
 
-server.listen(port, localhost , () => {   
+server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
